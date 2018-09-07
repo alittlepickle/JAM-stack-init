b2e5692d262cf84108f918d31e4cf00b8a562966
alittlepickle <ezio.crenshaw.giffaw@gmail.com>
2018-09-07 22:33:06 +0530

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Hi people, I'm Akshay</h1>
+    <p>This is a blank site</p>
+    <p>Prepping for JAMStack Hackathon.</p>
+    <Link to="/page-2/">Go to page 2</Link>
+  </div>
+)
+
+export default IndexPage
